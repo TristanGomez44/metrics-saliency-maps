905bc47d2ade804b6e677c48775483b7df08bca8
E144069X <tristan.gomez@etu.univ-nantes.fr>
2022-10-05 09:34:34 +0200

    Added multi_step metrics and tests

---
+++ multi_step_metrics.py
@@ -0,0 +1,150 @@
+import sys
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+def compute_corr_metric(score_var, all_sal_score_list):
+    corr_list = []
+    for i in range(len(score_var)):
+        var_class_score = score_var[i][:,np.newaxis]
+        sal_score = all_sal_score_list[i][:,np.newaxis]
+
+        points = np.concatenate((var_class_score,sal_score),axis=-1)
+        corr_list.append(np.corrcoef(points,rowvar=False)[0,1])
+
+    corr_mean = np.array(corr_list).mean()
+    return corr_mean
+
+def compute_auc_metric(all_score_list):
+    auc_list = []
+    for i in range(len(all_score_list)):
+        scores = all_score_list[i]
+        auc = np.trapz(scores,np.arange(len(scores))/(len(scores)-1))
+        auc_list.append(auc)
+
+    auc_mean = np.array(auc_list).mean()
+    return auc_mean
+
+def blur_data(data):
+    kernel_size = max(data.shape[2]//5,9)
+    kernel = torch.ones(kernel_size,kernel_size)
+    kernel = kernel/kernel.numel()
+    kernel = kernel.unsqueeze(0).unsqueeze(0).expand(3,1,-1,-1)
+    kernel = kernel.to(data.device)
+    data = F.conv2d(data,kernel,padding=kernel.size(-1)//2,groups=kernel.size(0))  
+    return data
+
+class AUC_Metric():
+
+    def __init__(self,data_shape,explanation_shape,bound_max_step=True):
+
+        #Set this to true to limit the maximum number of inferences computed by the metric
+        #The DAUC/IAUC protocol requires one inference per pixel of the explanation map.
+        #This can be prohibitive for high resolution maps like Smooth-Grad.
+        #In the case of a high-resolution map (>14x14), setting this arg to True results 
+        #in one inference for every few pixels removed, instead of one inference per pixel.
+        self.bound_max_step = bound_max_step 
+
+        self.size_ratio = data_shape[3]//explanation_shape[3]
+        self.total_pixel_nb = explanation_shape[2]*explanation_shape[3]
+        self.step_nb = min(14*14,self.total_pixel_nb) if self.bound_max_step else self.total_pixel_nb
+        self.pixel_removed_per_step = self.total_pixel_nb//self.step_nb
+
+    def update_data(self,data, i, y1, y2, x1, x2,data_to_replace_with):
+        data[i:i+1,:,y1:y2,x1:x2] = data_to_replace_with[i:i+1,:,y1:y2,x1:x2]
+        return data
+
+    def data_to_replace_with(data):
+        raise NotImplementedError
+
+    def preprocess_data(data):
+        raise NotImplementedError
+
+    def compute_metric(self,all_score_list,all_sal_score_list):
+        return compute_auc_metric(all_score_list)
+
+    def __call__(self,model,data,explanations,class_to_explain_list=None):
+
+        data_to_replace_with = self.init_data_to_replace_with(data)
+        data = self.preprocess_data(data)        
+
+        all_score_list = []
+        all_sal_score_list = []
+
+        for i in range(len(data)):
+            
+            if class_to_explain_list is None:
+                class_to_explain = torch.argmax(model(data[i:i+1]),axis=1)[0]
+            else:
+                class_to_explain = class_to_explain_list[i]
+        
+            expl = explanations[i:i+1]
+            left_pixel_nb = self.total_pixel_nb
+
+            output = model(data[i:i+1])[0,class_to_explain]
+
+            score_list = [output]
+            saliency_score_list = []            
+
+            while left_pixel_nb > 0:
+                saliency_scores,ind_max = expl[0,0].view(-1).topk(k=self.pixel_removed_per_step)
+                ind_max = ind_max[:left_pixel_nb]
+                x_max,y_max = ind_max % expl.shape[3],torch.div(ind_max,expl.shape[3],rounding_mode="floor")
+                
+                for j in range(self.pixel_removed_per_step):
+                    x1,y1 = x_max[j]*self.size_ratio,y_max[j]*self.size_ratio,
+                    x2,y2 = x1+self.size_ratio,y1+self.size_ratio
+                    
+                    data = self.update_data(data,i,y1,y2,x1,x2,data_to_replace_with)
+
+                    expl[i:i+1,:,y_max[j],x_max[j]] = -1                       
+
+                left_pixel_nb -= self.pixel_removed_per_step
+                output = model(data[i:i+1])[0,class_to_explain]
+                score_list.append(output.item())
+                saliency_score_list.append(saliency_scores.mean())
+
+            all_score_list.append(score_list)
+            all_sal_score_list.append(saliency_score_list)
+
+        all_score_list = np.array(all_score_list)
+        all_sal_score_list = np.array(all_sal_score_list)
+
+        mean = self.compute_metric(all_score_list,all_sal_score_list)
+        return mean
+
+class DAUC(AUC_Metric):
+    def __init__(self,data_shape,explanation_shape,bound_max_step):
+        super().__init__(data_shape,explanation_shape,bound_max_step)
+    
+    def init_data_to_replace_with(self,data):
+        return torch.zeros_like(data,device=data.device)
+
+    def preprocess_data(self,data):
+        return data
+
+class IAUC(AUC_Metric):
+    def __init__(self,data_shape,explanation_shape,bound_max_step):
+        super().__init__(data_shape,explanation_shape,bound_max_step)
+    
+    def init_data_to_replace_with(self,data):
+        return data
+
+    def preprocess_data(self,data):
+        return blur_data(data)
+
+class DC(DAUC):
+    def __init__(self,data_shape,explanation_shape,bound_max_step):
+        super().__init__(data_shape,explanation_shape,bound_max_step)
+    
+    def compute_metric(self, all_score_list, all_sal_score_list):
+        score_var = all_score_list[:,:-1] - all_score_list[:,1:] 
+        return compute_corr_metric(score_var, all_sal_score_list)
+
+class IC(IAUC):
+    def __init__(self,data_shape,explanation_shape,bound_max_step):
+        super().__init__(data_shape,explanation_shape,bound_max_step)
+    
+    def compute_metric(self, all_score_list, all_sal_score_list):
+        score_var = all_score_list[:,1:] - all_score_list[:,:-1]
+        return compute_corr_metric(score_var, all_sal_score_list)
---
+++ test_multi_step_metrics.py
@@ -0,0 +1,189 @@
+import torch
+import numpy as np
+from multi_step_metrics import DAUC,IAUC,DC,IC,blur_data
+
+#Test a model producing a linearly decreasing score from 1 to 0
+def get_dauc1():
+    test_dic = {}
+    test_dic["data"] = torch.ones(1,3,56,56)
+    test_dic["expl"] = test_dic["data"].clone()
+    test_dic["metrConst"] = DAUC
+    def test_model_dauc_1(x):
+        res_neg = (x==0).sum(dim=3).sum(dim=2).sum(dim=1,keepdim=True)
+        res_pos = (x==1).sum(dim=3).sum(dim=2).sum(dim=1,keepdim=True)
+        res = torch.cat((res_neg,res_pos),dim=1)
+        res = res/res.sum(dim=1,keepdim=True)
+        return res
+    test_dic["model"] = test_model_dauc_1
+    test_dic["class_to_explain"] = torch.ones(len(test_dic["data"])).long()
+    test_dic["target"] = 0.5000
+    return test_dic 
+
+#Test a model producing a approximately linearly increasing score from 0 to 1
+def get_iauc1():
+    test_dic = get_dauc1().copy()
+    test_dic["metrConst"] = IAUC
+    torch.manual_seed(0)
+    data_shape = test_dic["data"].shape
+    data = torch.rand(size=(data_shape[0],3,data_shape[2],data_shape[3]))
+    test_dic["data"] = data
+    test_dic["expl"] = test_dic["data"].clone()
+    data_blur_std_iauc1 = blur_data(data).std().clone()
+    data_std_iauc1 = data.std().clone()
+    data_ref_iauc1 = data.clone()
+    def test_model_iauc_1(x):
+        res_neg = (x!=data_ref_iauc1).sum(dim=3).sum(dim=2).sum(dim=1,keepdim=True)
+        res_pos = (x==data_ref_iauc1).sum(dim=3).sum(dim=2).sum(dim=1,keepdim=True)
+        res = torch.cat((res_neg,res_pos),dim=1)
+        res = res/res.sum(dim=1,keepdim=True)
+        return res
+    test_dic["model"] = test_model_iauc_1
+    test_dic["target"] = 0.5000
+    return test_dic
+
+#Test a model producing a constant score
+def get_dauc2():
+    test_dic = {}
+    data = torch.ones(1,3,56,56)
+    test_dic["data"] = data.clone()
+    test_dic["expl"]  = test_dic["data"].clone()
+    test_dic["metrConst"] = DAUC
+    torch.manual_seed(2)
+    rand_nb = torch.rand(size=(1,)).item()
+    test_dic["model"]  = lambda x:rand_nb*torch.ones(x.shape[0],2)
+    test_dic["class_to_explain"]  = torch.ones(len(test_dic["data"])).long()
+    test_dic["target"] = rand_nb
+    return test_dic
+
+#Test a model producing a constant score
+def get_iauc2():
+    test_dic = get_dauc2()
+    test_dic = test_dic.copy()
+    test_dic["metrConst"] = IAUC
+    return test_dic
+
+#Test a fully uncalibrated model
+def get_dc1():
+    test_dic = {}
+    data = torch.ones(1,3,56,56)
+    test_dic["data"] = data.clone()
+    data_shape = test_dic["data"].shape
+    torch.manual_seed(2)
+    expl = torch.rand(size=(data_shape[0],1,data_shape[2],data_shape[3]))
+    test_dic["expl"] = expl.clone()
+    test_dic["metrConst"] = DC
+    def test_model_corr_metrics_1(x):
+        res = torch.rand(size=(data.shape[0],2))
+        return res
+    test_dic["model"] = test_model_corr_metrics_1  
+    test_dic["class_to_explain"] = torch.ones(data_shape[0]).long()
+    test_dic["target"] = 0.00
+    return test_dic 
+
+#Test a fully uncalibrated model
+def get_ic1():
+    test_dic = get_dc1()    
+    test_dic = test_dic.copy()
+    test_dic["metrConst"] = IC
+    return test_dic
+
+#Generates random scores and a correlated explanation
+def generate_random_scores_and_explanation(data_shape,metric):
+    #Constructing random score variation
+    torch.manual_seed(0)
+    score_var,_ = torch.rand(size=(data_shape[2]*data_shape[3],)).sort(descending=True)
+    score_var = score_var/score_var.sum()
+
+    #Constructing associated scores
+    scores = torch.zeros(score_var.shape[0]+1)
+    scores[0] = score_var.sum() if metric == "DC" else 0
+    
+    for i in range(1,len(scores)):
+        if metric == "DC":
+            scores[i] = scores[i-1] - score_var[i-1]
+        else:
+            scores[i] = score_var[i-1] + scores[i-1]
+
+    #Constructing correlated explanation
+    torch.manual_seed(0)
+    a,b = torch.rand(2)
+    expl = a*score_var+b
+    expl = expl.view(1,1,data_shape[2],data_shape[3])
+
+    return scores,expl 
+
+#Test a fully calibrated model
+def get_dc2():
+        
+    test_dic = {}
+    data = torch.ones(1,3,4,4)
+    scores,expl = generate_random_scores_and_explanation(data.shape,"DC")
+
+    metrConst = DC
+
+    def test_model_dc_2(x):
+        x = x[:,0:1].view(-1)
+        inds = torch.argwhere(x==0)
+        res = torch.zeros(1,2).float() 
+        if len(inds) == 0:
+            res[0,0] = scores[0]
+            return res
+        else:
+            res[0,0] = scores[inds[-1,0]+1]
+        return res
+
+    model = test_model_dc_2  
+    class_to_explain = torch.zeros(len(data)).long()            
+
+    test_dic["data"] = data.clone()
+    test_dic["expl"] = expl.clone()
+    test_dic["metrConst"] = DC
+    test_dic["model"] = model 
+    test_dic["class_to_explain"] = class_to_explain
+    test_dic["target"] = 0.9999
+    return test_dic
+
+def get_ic2():
+    test_dic = get_dc2()    
+    test_dic = test_dic.copy()
+    test_dic["metrConst"] = IC
+    scores_ic,expl = generate_random_scores_and_explanation(test_dic["data"].shape,"IC")
+    test_dic["expl"] = expl
+    data_ref = test_dic["data"].clone()
+    def test_model_ic_2(x):
+        x = x[:,0:1].view(-1)
+        data_flat = data_ref[:,0:1].view(-1)
+        inds = torch.argwhere(x==data_flat)
+        res = torch.zeros(1,2).float() 
+        if len(inds) == 0:
+            res[0,0] = scores_ic[0]
+            return res
+        else:
+            res[0,0] = scores_ic[inds[-1,0]+1]
+        return res
+    test_dic["model"] = test_model_ic_2
+    test_dic["target"] = 0.9999
+    return test_dic
+
+if __name__ == "__main__":
+ 
+    all_test_dic = {}
+    all_test_dic["DAUC1"] = get_dauc1()
+    all_test_dic["IAUC1"] = get_iauc1()
+    all_test_dic["DAUC2"] = get_dauc2()
+    all_test_dic["IAUC2"] = get_iauc2()
+    
+    all_test_dic["DC1"] = get_dc1()
+    all_test_dic["IC1"] = get_ic1()
+    all_test_dic["DC2"] = get_dc2()
+    all_test_dic["IC2"] = get_ic2()
+ 
+    test_to_do = ["DAUC1","IAUC1","DAUC2","IAUC2","DC1","IC1","DC2","IC2"]
+    
+    for test in test_to_do:
+        torch.manual_seed(0)
+        dic = all_test_dic[test]
+        metric = dic["metrConst"](dic["data"].shape,dic["expl"].shape,True)
+        mean = metric(dic["model"],dic["data"].clone(),dic["expl"].clone(),dic["class_to_explain"])
+        sucess = np.abs(mean - dic["target"]) < 0.01
+        print(f"Test: {test}, Result:{mean}, Target:{dic['target']}, Sucess:{sucess}")
